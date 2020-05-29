c4214b5dca1cdf30c58fe01e929a32f00ee0359e
AtalH <hatal@qq.com>
2020-05-29 23:29:46 +0800

    sovle 258 digits root

---
+++ java/258digitsRoot/258.各位相加.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=258 lang=java
+ *
+ * [258] 各位相加
+ */
+
+// @lc code=start
+class Solution {
+    /**
+     * 通俗解释：https://leetcode-cn.com/problems/add-digits/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-5-7/
+     * 原数: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
+     * 数根: 1 2 3 4 5 6 7 8 9  1  2  3  4  5  6  7  8  9  1  2  3  4  5  6  7  8  9  1  2  3 
+     * 
+     */
+    public int addDigits(int num) {
+        return (num - 1) % 9 +1;
+    }
+}
+// @lc code=end
+
