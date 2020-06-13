ffeeebfa880b90396f2abb93bff2e7cea6039c09
AtalH <hatal@qq.com>
2020-06-13 11:52:48 +0800

    solve 367 use Newton Iteration

--- go/367isPerfectSquare/367.有效的完全平方数.go
+++ go/367isPerfectSquare/367.有效的完全平方数.go
@@ -9,12 +9,19 @@ func isPerfectSquare(num int) bool {
 	// 完全平方数是指 s = n*n, n 是整数
 	// 解法1. 二分查找，时间复杂度 O(log n)
 	// 解法2. 利用奇数数列和公式 1 + 3 + 5 + ... + (2N−1) = n*n , 时间复杂度 O(sqrt(N))
-	odd := 1
-	for num > 0 {
-		num -= odd
-		odd += 2
+	// 解法3. 牛顿迭代法，利用 s = n*n --> x = (x + s/x)/2 使得 x 不断收敛于 n
+	if num < 0 {
+		return false
+	} else if num <= 1 {
+		// 0 & 1
+		return true
 	}
-	return num == 0
+	// 初始值选取
+	x := num / 2
+	for x * x > num {
+		x = (x + num / x) / 2
+	}
+	return x * x == num
 }
 // @lc code=end
 
