5780c3416ba15cd6e110d229c9e369f73ecee176
AtalH <hatal@qq.com>
2020-06-14 21:16:46 +0800

    solve 169 use sorting

--- go/169majorityElement/169.多数元素.go
+++ go/169majorityElement/169.多数元素.go
@@ -6,18 +6,8 @@
 
 // @lc code=start
 func majorityElement(nums []int) int {
-	m := len(nums)/2
-    countMap := make(map[int]int, m)
-    for _, v := range nums {
-        count := countMap[v]
-        count++
-        if count > m {
-            return v
-        } else {
-            countMap[v] = count
-        }
-    }
-    panic("majority element dose not exist")
+    sort.Ints(nums)
+    return nums[len(nums)/2]
 }
 // @lc code=end
 
