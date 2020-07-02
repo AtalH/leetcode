ffe76001d70423ac470a1898a20890538cee9017
AtalH <hatal@qq.com>
2020-07-03 00:23:04 +0800

    no extra max func

--- go/53maxSubArray/53.最大子序和.go
+++ go/53maxSubArray/53.最大子序和.go
@@ -12,20 +12,16 @@ func maxSubArray(nums []int) int {
 	}
 	max, temp := nums[0], nums[0]
 	for i := 1; i < len; i++ {
-		temp =  maxInt(temp, 0) + nums[i]
+		if temp >= 0 {
+			temp += nums[i]
+		} else {
+			temp = nums[i]
+		}
 		if temp > max {
 			max = temp
 		}
 	}
 	return max
-}
-
-func maxInt(a int, b int) int {
-	if a >= b {
-		return a
-	} else {
-		return b
-	}
 }
 // @lc code=end
 
