0c5641d3aabdc03b935909919359b37d7402d886
AtalH <hatal@qq.com>
2020-07-03 00:31:28 +0800

    add comment

--- go/53maxSubArray/53.最大子序和.go
+++ go/53maxSubArray/53.最大子序和.go
@@ -10,6 +10,9 @@ func maxSubArray(nums []int) int {
 	if len == 0 {
 		return 0;
 	}
+	// 最大值问题，首先考虑是否适合使用DP
+	// dp status: dp[i] = max(nums[i], nums[i] + dp[i - 1])
+	// 如果当前元素加上之前的子序和更大，则加上，否则只用当前元素作为新的子序和
 	max, temp := nums[0], nums[0]
 	for i := 1; i < len; i++ {
 		if temp >= 0 {
