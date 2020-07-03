9ca239f3e95783419dc58e1e2f2bd696693a281b
AtalH <hatal@qq.com>
2020-07-03 23:36:55 +0800

    solve 108 sortedArrayToBST

---
+++ java/108sortedArrayToBST/108.将有序数组转换为二叉搜索树.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=108 lang=java
+ *
+ * [108] 将有序数组转换为二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return build(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode build(int[] nums, int left, int right) {
+        if(left > right) {
+            return null;
+        }
+        int mid = left + (right - left)/2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = build(nums, left, mid - 1);
+        node.right = build(nums, mid + 1, right);
+        return node;
+    }
+}
+// @lc code=end
+
