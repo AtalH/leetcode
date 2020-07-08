56454b31fb1a28f02cd944dc0d035b2f8d83f3a2
AtalH <hatal@qq.com>
2020-07-08 23:46:49 +0800

    optimize dp status

--- java/198house-robber/198.打家劫舍.java
+++ java/198house-robber/198.打家劫舍.java
@@ -14,13 +14,14 @@ class Solution {
         if (len == 1) {
             return nums[0];
         }
-        int[] dp = new int[len];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
+        int first = nums[0];
+        int second = Math.max(nums[0], nums[1]);
         for (int i = 2; i < len; i++) {
-            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+            int temp = second;
+            second = Math.max(first + nums[i], second);
+            first = temp;
         }
-        return dp[len - 1];
+        return second;
     }
 }
 // @lc code=end
